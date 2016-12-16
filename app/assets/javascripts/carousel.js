2638e623412c5535f452ffd2cb76cb2fa68121b8
Micah Britton <micahwkb@gmail.com>
2016-12-16 14:10:59 -0500

    carousel partial now working (Dee's creation)

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,7 +1,7 @@
-'use strict';
+//
+// for carousel form
+//
 
 $(document).ready(() => {
-
-$('.carousel.carousel-slider').carousel({full_width: true});
-
+  $('.carousel.carousel-slider').carousel({full_width: true});
 })
