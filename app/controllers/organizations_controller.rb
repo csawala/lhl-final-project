837397abd03009bbb5b5a408ff406299cd6c507b
Micah Britton <micahwkb@gmail.com>
2016-12-17 17:31:01 -0500

    minor tweak in org controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,8 +1,9 @@
 class OrganizationsController < ApplicationController
   def index
-    @organizations = Organization.all
+    @organizations = Organization.all.order(name: :asc)
   end
 
   def show
+    @org = Organization.find(params[:id])
   end
 end
