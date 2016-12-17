7457ba6465cba1e44e9e3a47762f5e83634d7b99
DeeKeilholz <deekeilholz@gmail.com>
2016-12-17 01:06:07 +0000

    changes to form carousel

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -5,4 +5,13 @@
 $(document).ready(() => {
 
   $('.carousel.carousel-slider').carousel({full_width: true});
+
+  $('#offer-button').click(function(q) {
+    $('.carousel').carousel('next')
+  })
+
+  $('#give-button').click(function(q) {
+    $('.carousel').carousel('next')
+  })
+
 })
