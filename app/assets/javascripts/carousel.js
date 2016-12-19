1149a1b0c9ea288462af76965103a0aa127172a2
DeeKeilholz <deekeilholz@gmail.com>
2016-12-19 23:34:23 +0000

    loads of css changes and carousel changes

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,17 +1,37 @@
 //
 // for carousel form
 //
+$(document).on('turbolinks:load', () => {
 
-// $(document).ready(() => {
-//
-//   $('.carousel.carousel-slider').carousel({full_width: true});
-//
-//   $('#offer-button').click(function(q) {
-//     $('.carousel').carousel('next')
-//   })
-//
-//   $('#individual-button').click(function(q) {
-//     $('.carousel').carousel('next')
-//   })
-//
-// })
+
+      $('.carousel.carousel-slider').carousel({full_width: true});
+
+      $('select').material_select();
+
+      $('#offer-button').click(function(q) {
+        $('.carousel').carousel('next')
+      })
+
+      $('#individual-button').click(function(q) {
+        $('.carousel').carousel('next')
+      })
+
+      $('#give-button-individual').click(function(q) {
+        $('.carousel').carousel('next')
+      })
+
+      $('#participate-button-individual').click(function(q) {
+        $('.carousel').carousel('next', 2)
+      })
+
+      $('#organization-button').click(function(){
+          window.location.href='/dashboard/1';
+      })
+
+     $('#causes_form').on('submit', function(e) {
+           e.preventDefault();
+           var causes = $('#multiple_select').serialize();
+           window.location = '/organizations?' + causes
+       });
+
+})
---
+++ app/assets/javascripts/homepage.js
@@ -0,0 +1,7 @@
+$(document).on('turbolinks:load', () => {
+
+  $('#homepage_get_started').click(function(){
+      window.location.href='/do_good';
+  })
+
+});
