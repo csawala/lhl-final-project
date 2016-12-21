d262922c79b653e93b5c5ebd3336e848dafdced6
csawala <chris.sawala@gmail.com>
2016-12-21 01:27:11 +0000

    finished org_edit page w/ full functionality

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,9 +1,49 @@
 class OrganizationsController < ApplicationController
+  # do we need an authorize here?
+
   def index
     @organizations = Organization.filter_by_params(params).order(name: :asc)
+  end
+
+  def create
+    @org = Organization.new(org_params)
+
+    if @org.save
+      redirect_to "/dashboard", notice: 'Organization Created!'
+    else
+      # May require better else here
+      render :root, notice: 'Something got mugged up!'
+    end
+  end
+
+  def update
+    org = Organization.find(params[:id])
+    org.update(org_params)
+
+    redirect_to "/dashboard"
   end
 
   def show
     @org = Organization.find(params[:id])
   end
+
+  private
+  def org_params
+    params.require(:organization).permit(
+    :name,
+    :address,
+    :suite,
+    :city,
+    :postal,
+    :province,
+    :country,
+    :email,
+    :primaryphone,
+    :secondaryphone,
+    :url,
+    :description,
+    :image
+    )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'application#index'
 
-  resources :organizations, only: [:index, :show, :create]
+  resources :organizations, only: [:index, :show, :create, :update]
 
   # resources :dashboard, only: [:show, :update]
 
