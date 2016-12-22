0d20bcf93609c0668ddc1a64bd45f5a37622048b
Micah Britton <micahwkb@gmail.com>
2016-12-21 20:30:14 -0500

    added migration for more data in goods_types join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220205546) do
+ActiveRecord::Schema.define(version: 20161222012649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20161220205546) do
     t.boolean  "urgent"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "description"
+    t.boolean  "active"
   end
 
   create_table "needs", force: :cascade do |t|
