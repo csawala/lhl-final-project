128b32850312d89decba5c46b3ea118bac9e2a3e
Micah Britton <micahwkb@gmail.com>
2017-01-01 17:33:04 -0500

    fixed footer placement using materialize css, added highlighting function on filter buttons for needs/offers pages

--- app/assets/javascripts/offers.js
+++ app/assets/javascripts/offers.js
@@ -1,2 +1,27 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+'use strict'
+
+
+const swapColourClass = (elem, target) => {
+  elem.closest(target).removeClass('lighten-1')
+  elem.closest(target).addClass('darken-3')
+}
+
+const highlightFilterButton = () => {
+  const searchURL  = window.location.search
+  let $sortButtons = $('input.waves-button-input')
+
+  $sortButtons.each((_, button) => {
+    // console.log(button)
+    let filterText = button.value.split(' ').join('+')
+    console.log(filterText)
+    if (searchURL.includes(`=${filterText}`)) {
+      swapColourClass($(button), 'i')
+    }
+  })
+}
+
+window.onload = () => {
+
+  highlightFilterButton()
+
+}
