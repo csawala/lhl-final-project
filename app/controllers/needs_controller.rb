3d81709830771ea34e37ac230735093eed6a259f
Micah Britton <micahwkb@gmail.com>
2017-01-02 13:58:32 -0500

    tiny tweaks to code style

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -7,6 +7,7 @@ class NeedsController < ApplicationController
   def index
     @needs = GoodsTypesOrganization.where(needs: true, active: true)
                                    .filter_by_params(params)
+
     @goods_types = GoodsType.select(:name).all.order(name: :asc)
   end
 
